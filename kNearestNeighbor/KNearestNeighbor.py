Fixed a bug which prevent the label function to determine the nearest labels for each image. Now, the first variant with distance calculation via euclidean distance works fine.

kNearestNeighbor/KNearestNeighbor.py
@@ -45,6 +45,13 @@ class KNearestNeighbor(object):
         return self.predict_labels(dists, k=k)
 
     def euclideanDistance(self, img_train, img_test):
+        """
+        Calculate euclidean instance between img_train with n images and img_test with m images and store in
+        a n x m dimensional array
+        :param img_train:
+        :param img_test:
+        :return:
+        """
         distance = 0
         index = 0
         for data in img_train:
@@ -78,7 +85,8 @@ class KNearestNeighbor(object):
         #####################################################################
         for i in range(num_test):
             for j in range(num_train):
-                dists[i, j] = sum(abs(self.X_train[j] - X[i]))
+                    dists[i, j] = np.sum(np.abs(self.X_train[j] - X[i]))
+            print(str(i) + 'th image distances calculated')
         #####################################################################
         #                       END OF YOUR CODE                            #
         #####################################################################
@@ -146,7 +154,15 @@ class KNearestNeighbor(object):
             # neighbors. Store these labels in closest_y.                           #
             # Hint: Look up the function numpy.argsort.                             #
             #########################################################################
-            
+            #determine k image indexes which have the smallest euclidean distance to the ith test image
+            k_nearest_training_image_indexes = np.argsort(dists[i])[0:k]
+            for index in k_nearest_training_image_indexes:
+                closest_y.append(self.y_train[index])
+            print(str(i) + 'images analyzed :)')
+            y_pred[i] = (closest_y[np.argmax(closest_y)])
+
+
+
             #########################################################################
             # TODO (2):                                                             #
             # Now that you have found the labels of the k nearest neighbors, you    #

kNearestNeighbor/loadAndprocessData.py
@@ -155,13 +155,13 @@ def plotSampleImages(images, labels):
 #####################################################################
 
 # Subsample trainings data
-num_training = 3 #5000
+num_training = 5000 #5000
 mask = range(num_training)
 X_train = X_train[mask]
 y_train = y_train[mask]
 
 # Subsample test data
-num_test = 2 #500
+num_test = 500 #500
 mask = range(num_test)
 X_test = X_test[mask]
 y_test = y_test[mask]
@@ -196,6 +196,7 @@ classifier.train(X_train, y_train)
 dists = classifier.compute_distances_with_loops(X_test)
 print(dists.shape) # Should be: (500, 5000)
 
+'''
 # This task is not vital for the notebook. Run that cell only if you have implement the vectorized function
 dists_vec = classifier.compute_distances_vectorized(X_test)
 
@@ -206,3 +207,12 @@ if difference < 0.001:
     print('Good! The distance matrices are the same')
 else:
     print('Uh-oh! The distance matrices are different')
+'''
+
+# Before running this cell: implement the function predict_labels
+y_test_pred = classifier.predict_labels(dists,5)
+
+# Compute and print the fraction of correctly predicted examples
+num_correct = np.sum(y_test_pred == y_test)
+accuracy = float(num_correct) / num_test
+print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))